8913865e1d3d133e7225f55ced28fe9fd64ed034
Michael Dorst <m@mdorst.net>
2019-04-07 17:48:35 -0700

    Implement PGU

--- pgu.v
+++ pgu.v
@@ -1,36 +1,8 @@
-module pgu (
-    a0,
-    a1,
-    a2,
-    a3,
-    b0,
-    b1,
-    b2,
-    b3,
-    p0,
-    p1,
-    p2,
-    p3,
-    g0,
-    g1,
-    g2,
-    g3
-)
 
-input a0;
-input a1;
-input a2;
-input a3;
-input b0;
-input b1;
-input b2;
-input b3;
+module pgu (input [3:0] a, b, output [3:0] p, g)
 
-output p0;
-output p1;
-output p2;
-output p3;
-output g0;
-output g1;
-output g2;
-output g3;
+assign g = a & b;
+
+assign p = a ^ b;
+
+endmodule
