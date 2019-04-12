fd6508f8985ea48bdb417c0664e1cfb6ebb3c220
Michael Dorst <m@mdorst.net>
2019-04-11 18:30:05 -0700

    Add ovf to adder

--- adder.v
+++ adder.v
@@ -1,12 +1,12 @@
 // adder.v
 
-module adder(cin, a, b, s, cout);
+module adder(cin, a, b, s, cout, ovf);
 
     input cin;
     input [3:0] a, b;
 
     output [3:0] s;
-    output cout;
+    output cout, ovf;
 
     wire [3:0] p, g, c;
 
@@ -17,5 +17,7 @@ module adder(cin, a, b, s, cout);
     sum sum(cin, c[2:0], p, s);
 
     assign cout = c[3];
+
+    xor(ovf, c[3], c[2]);
 
 endmodule
